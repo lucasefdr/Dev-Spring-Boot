afaa97ee735deff6b1e39b788cfcc38346f97306
Lucas <lucas.efdrosa@gmail.com>
2023-04-01 14:51:24 -0300

    Dependency Injection with Autowired

--- src/main/java/io/github/springboot/controller/MovieController.java
+++ src/main/java/io/github/springboot/controller/MovieController.java
@@ -1,6 +1,8 @@
 package io.github.springboot.controller;
 
 import io.github.springboot.domain.Movie;
+import io.github.springboot.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,10 @@ import java.util.List;
 @RestController
 @RequestMapping("movies")
 public class MovieController {
+    // Dependency Injection com Autowired
+    @Autowired
+    private DateUtil dateUtil;
+
     // localhost:8080/movies/
     @GetMapping(path = "list")
     public List<Movie> listMovies() {
