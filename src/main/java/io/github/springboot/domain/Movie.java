54c013359ab21c15798e796b0aeff8c00221b6e0
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-04-28 15:40:10 -0300

    Change Movie class

--- src/main/java/io/github/springboot/domain/Movie.java
+++ src/main/java/io/github/springboot/domain/Movie.java
@@ -1,11 +1,20 @@
 package io.github.springboot.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
