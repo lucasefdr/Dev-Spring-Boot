ef1ddf5de266313d4bdbc3e6149e30f662c1f253
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-04-28 22:08:55 -0300

    studies: JpaRepository

--- src/main/java/io/github/springboot/repository/MovieRepository.java
+++ src/main/java/io/github/springboot/repository/MovieRepository.java
@@ -1,9 +1,9 @@
 package io.github.springboot.repository;
 
 import io.github.springboot.domain.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MovieRepository {
-    List<Movie> listAll();
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
--- src/main/java/io/github/springboot/service/MovieService.java
+++ src/main/java/io/github/springboot/service/MovieService.java
@@ -1,45 +1,41 @@
 package io.github.springboot.service;
 
 import io.github.springboot.domain.Movie;
+import io.github.springboot.repository.MovieRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class MovieService {
-    private static List<Movie> movies;
+    private final MovieRepository repository;
 
-    static {
-        movies = new ArrayList<>(List.of(new Movie(1L, "The Lord of the Rings"), new Movie(2L, "PS: I Love You"), new Movie(3L, "Revengers")));
+    public MovieService(MovieRepository repository) {
+        this.repository = repository;
     }
 
     public List<Movie> listAll() {
-        return movies;
+        return repository.findAll();
     }
 
     public Movie findById(Long id) {
-        return movies.stream()
-                .filter(movie -> movie.getId().equals(id))
-                .findFirst()
+        return repository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Movie not found"));
     }
 
     public Movie save(Movie movie) {
-        movie.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
-        movies.add(movie);
-        return movie;
+        return repository.save(movie);
     }
 
     public void delete(Long id) {
-        movies.remove(findById(id));
+        repository.deleteById(id);
     }
 
     public void replace(Movie movie) {
         delete(movie.getId());
-        movies.add(movie);
+        repository.save(movie);
     }
 }
